3eea2c62b66856bcd3748c3d8cdd467165ff9d2d
tryeverything36 <no-reply@email.com>
2025-04-28 15:10:20 -0700

    Second JNI function added

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,10 +3,18 @@
 
 extern "C" JNIEXPORT jstring
 
-JNICALL
+
 Java_com_ndktest_ndktest_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++, David";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jstring
+Java_com_ndktest_ndktest_MainActivity_stringFromJNI2(
+        JNIEnv *env,
+jobject /* this */) {
+std::string hello = "Hello from C++, New JNI";
+return env->NewStringUTF(hello.c_str());
 }
